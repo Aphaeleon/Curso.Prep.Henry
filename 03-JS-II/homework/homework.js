9b43780ba600d7c4e5723f76193416da3e96126e
Aphaeleon <davalilloleonardo@gmail.com>
2021-02-17 04:54:22 -0300

    HW3 is done now!

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,22 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x > y) {
+    return x;
+  }
+    else {
+      return y; 
+   }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+if (edad >= 18) {
+  return ("Allowed");
+}
+  else return ("Not allowed")
 }
   
 function conection(status) {
@@ -19,6 +29,13 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return ("Online");
+  }
+    if (status === 2) {
+      return ("Away");
+    }
+  else return ("Offline");
 }
 
 function saludo(idioma) {
@@ -28,6 +45,16 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma === "aleman") {
+    return ("Guten Tag!");
+    }
+    if (idioma === "mandarin") {
+      return ("Ni Hao!");
+    }
+    if (idioma === "ingles") {
+      return ("Hello!");
+    }
+    else return ("Hola!")
 }
 
 function colors(color) {
@@ -38,18 +65,45 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+   switch (color) {
+     case "blue": 
+      return ("This is blue");
+     case "red": 
+      return ("This is red");
+     case "green": 
+      return ("This is green");
+     case "orange":
+       return ("This is orange");
+     default: 
+      return ("Color not found");
+   }
 }
+//Leo: usé un ejemplo de w3Schools.com para aprender como funcionaba este comando.
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero === 10 || numero === 5) {
+    return true;
+  }
+    else {
+      return false;
+    }
 }
+//Leo: Por qué no se puede usar num === 10 && 5 para resolver este ejercicio?
+// Por qué de deben separar las variables? Me gustaria saber!
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20) {
+    return true;
+  }
+    else {
+      return false;
+    }
 }
 
 function esEntero(numero) {
@@ -60,6 +114,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return numero % 1 === 0;
 }
 
 function fizzBuzz(numero) {
@@ -67,7 +122,18 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 15 === 0) {
+    return "fizzbuzz";
+  }
+  if (numero % 5 === 0) {
+      return "buzz";
+    }
+  if (numero % 3 === 0) {
+    return "fizz";
+  }
+  return numero;
 }
+//Leo: todavia no me queda claro por qué la posicion de las formulas es tan importante en el calculo de las variables.
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
@@ -75,8 +141,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //Si no se cumplen ninguna de las condiciones anteriores, retornar false.
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  }else if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error"
+  }else if (num1 > 0 && num1 > num2 && num1 > num3) {
+    return "Número 1 es mayor y positivo";
+  }else if (num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }else {
+    return false;
+  }
 }
+// Leo: Debo aprender como se maneja la logica/orden de las condiciones para evitar errores de precedencia.
+// Intenté intercambiar la posicion de las formulas y logré que no me diera error. Ahora no se si es lo mas 
+// optimo o no. Me podrian explicar un poco mas? Gracias! 
 
 function esPrimo(numero) {
   // Devuelve "true" si "numero" es primo
@@ -84,35 +164,67 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+if (numero < 2) {
+  return false;
 }
+let root = Math.ceil(Math.sqrt(numero));
+for (let i = 2; i <= root ; i++) {
+  if (numero % i === 0) {
+    return false;
+  }
+} return true;
+}
+//Leo: desde el punto de vista de programacion, este fue el ejercicio mas dificil de todos!
+//Revisé algunos videos de YT y aparentemente usando la raiz cuadrada del numero
+//se logran evitar excesivos calculos para encontrar el numero primo que se quiere obtener.
+//Me gustaria que me explicaran esto mas en detalle porque parece ser una funcion importante.
 
 function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+if (valor > 0) {
+  Boolean (valor);
+  return "Soy verdadero";
 }
-
+  else {
+    return "Soy falso";
+  }
+}
+//Leo: tomé como referencia la explicacion que encontré en w3schools para resolver este ejercicio.
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+let tablaDelSeis = []
+for (let j = 0; j < 11; j++) {
+  tablaDelSeis.push(j * 6)
 }
+return tablaDelSeis 
+}
+//Leo:  necesito practicar mucho esta funcion porque no me siento confiado todavia en sus diversos usos.
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+return (numero < 1000 && numero > 99);
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  let x = numero;
+  let y = 0;
+  do {
+    x = x + 5;
+    y = y + 1;
+  }
+  while (y < 8);
+  return x;
 }
-
-
+//Leo : esta funcion no fue muy sencilla de resolver porque tuve que recurrir a codeacademy para
+// comprender como funcionaba la funcion do/while.
 // No modificar nada debajo de esta línea
 // --------------------------------
 
